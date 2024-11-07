1ad575f0f0adbab0044f9095e24665ce5b0f5edd
progresso85 <martin.heuripeau@gmail.com>
2024-11-07 12:37:05 +0100

    change function Heath bar ans PlayerHealth

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,20 +1,58 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class LifeSystem : MonoBehaviour
 {
-    public Slider slider;
+    public int maxLives = 5;         
+    public int currentLives;          
 
-    public void SetMaxHealth(int health)
+    public GameObject heartPrefab;   
+    public Transform content;        
+
+    private List<GameObject> hearts = new List<GameObject>();  // List for storing active cores
+
+    private void Start()
     {
-        slider.maxValue = health;
-        slider.value = health;
+        currentLives = maxLives;      
+        UpdateHeartsUI();            
     }
 
-    public void SetHealth(int health)
+    // Function to update the cores displayed
+    public void UpdateHeartsUI()
     {
-        slider.value = health;
+        // Deletes all current cores to reset them
+        foreach (var heart in hearts)
+        {
+            Destroy(heart);  // Deletes each object from the list
+        }
+        hearts.Clear();
+
+        // Adds a heart for each current hit point
+        for (int i = 0; i < currentLives; i++)
+        {
+            GameObject newHeart = Instantiate(heartPrefab, content);  // Creates a new heart in the container
+            hearts.Add(newHeart);  // Adds the heart to the list for future management
+        }
     }
 
+    // Function to add a life
+    public void AddLife()
+    {
+        if (currentLives < maxLives)
+        {
+            currentLives++;  
+            UpdateHeartsUI(); 
+        }
+    }
+
+    // Fonction pour perdre une vie
+    public void LoseLife()
+    {
+        if (currentLives > 0)
+        {
+            currentLives--;  
+            UpdateHeartsUI();  
+        }
+    }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,22 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth;
+    public int maxHealth = 5;          
+    private int currentHealth;         
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
-        HealthBar.SetMaxHealth(maxHealth);
-    }
+    private LifeSystem lifeSystem;     
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
+        currentHealth = maxHealth;
+
+       
+        lifeSystem = FindObjectOfType<LifeSystem>();
+        if (lifeSystem != null)
+        {
+            
+            lifeSystem.maxLives = maxHealth;
+            lifeSystem.currentLives = currentHealth;
+            lifeSystem.UpdateHeartsUI();
+        }
+    }
+
+    // Function to inflict damage on the player
+    public void TakeDamage(int damage)
+    {
+        if (currentHealth > 0)  
+        {
+            currentHealth -= damage;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);  
+
+            
+            if (lifeSystem != null)
+            {
+                lifeSystem.currentLives = currentHealth;  
+                lifeSystem.UpdateHeartsUI();
+            }
+
+            
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    // Function to heal the player
+    public void Heal(int healAmount)
+    {
+        if (currentHealth < maxHealth)  
+        {
+            currentHealth += healAmount;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+            
+            if (lifeSystem != null)
+            {
+                lifeSystem.currentLives = currentHealth;  
+                lifeSystem.UpdateHeartsUI();
+            }
+        }
+    }
+
+    // Function called when the player dies
+    private void Die()
+    {
+        Debug.Log("Le joueur est mort !");
+        // Ajoute ici la logique de mort, comme recharger la scène, afficher un écran de Game Over, etc.
     }
 }
